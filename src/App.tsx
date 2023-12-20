6c59fe2df69c6a5cacbbac0d7df562b424811c46
thesteemguru <theguru.asia@gmail.com>
2023-12-20 14:04:22 +0530

    Installing Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
 }
 
 export default App;

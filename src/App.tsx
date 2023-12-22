ebd61a69c8e13fb1bf3835de0eecb4ba080fa819
thesteemguru <theguru.asia@gmail.com>
2023-12-22 10:45:35 +0530

    Displaying the Genres

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,20 @@ function App() {
   return (
     <>
       <Grid
-        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: `1fr`,
+          lg: `200px`,
+        }}
       >
         <GridItem area="nav">
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside">
+          <GridItem area="aside" paddingX={5}>
             <GenreList />
           </GridItem>
         </Show>
